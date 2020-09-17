120b99989f7165785044472a86e9e3951fb9779b
Leoxyh <58529404+Leoxyh@users.noreply.github.com>
2020-09-17 19:25:40 -0400

    Add files via upload

---
+++ Functions Assessment_ICS4U.py
@@ -0,0 +1,25 @@
+def the_number_five():
+    return 5
+    pass
+
+
+def so_useful(x):
+    return x
+    pass
+
+
+def add_two(x):
+    return x + 2
+    pass
+
+
+def say_hello(Dave):
+    pass
+
+
+def sum_list(x):
+    pass
+
+
+def find_item_list(x):
+    pass
